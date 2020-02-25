4ae088f4525efc4846d78f099b4dbcf7c6541425
Natalya Despain <10792929@uvu.edu>
2020-02-24 18:36:18 -0700

    Added foo

---
+++ src/FooFighters.php
@@ -0,0 +1,13 @@
+<?php
+class FooFighters {
+    public $foo;
+    public function __construct() {
+         $this->foo = array();
+         $this->foo[] = 'foo';
+         $this->foo[] = 'bar';
+    }
+    public function getRandomFoo() {
+         $random = random_int(0, count($this->foo)-1);
+         return $this->foo[$random];
+    }
+}
